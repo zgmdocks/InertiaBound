582830760f617aca2e8f12b1832e9770ce65f6b2
Zach <z.dockstader@gmail.com>
2017-06-27 12:52:27 -0400

    finished script

--- Restart.sh
+++ Restart.sh
@@ -1,13 +1,21 @@
 file="DeleteResults.txt"
 
+sage Delete.sage
+sleep 15
+
 prevlast=$(cat $file | tail -n 2)
 
 while true
 do
-    sleep 30m
+    sleep 30
     if [ prevlast -ne $(cat $file | tail -n 2 ]
     then
-        $(kill -INT ps | grep 'python Delete.sage.py' | grep -v 'grep' | grep -v 'bash'| awk '{print $1;}')
+        kill -INT $(ps | grep 'python Delete.sage.py' | grep -v 'grep' | grep -v 'bash'| awk '{print $1;}')
+        ./cleaner.py
+        git add .
+        git commit -m "added new graphs"
+        git push
+        sage Delete.sage
     fi
 done
 
