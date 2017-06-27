abdb85bf9a6710b97d57876335b608bf319fd7c0
Zach <z.dockstader@gmail.com>
2017-06-27 13:18:08 -0400

    added new graphs

--- Restart.sh
+++ Restart.sh
@@ -1,21 +1,29 @@
 file="DeleteResults.txt"
 
-sage Delete.sage
+/../Applications/SageMath-7.6.app/sage Delete.sage &
 sleep 15
-
-prevlast=$(cat $file | tail -n 2)
-
+prevlast=$(cat $file | tail -n 2 | head -1)
+echo $prevlast
 while true
 do
-    sleep 30
-    if [ prevlast -ne $(cat $file | tail -n 2 ]
+    echo "NOW"
+    sleep 10
+    if [ "$prevlast" == "$(cat $file | tail -n 2 | head -1)" ]
     then
+        echo ""
+        echo "Program will be restarted"
+        echo ""
         kill -INT $(ps | grep 'python Delete.sage.py' | grep -v 'grep' | grep -v 'bash'| awk '{print $1;}')
         ./cleaner.py
         git add .
         git commit -m "added new graphs"
         git push
-        sage Delete.sage
+        /../Applications/SageMath-7.6.app/sage Delete.sage &
+    else
+        echo ""
+        echo "Program has made progress"
+        echo ""
+        prevlast=$(cat $file | tail -n 2 | head -1)
     fi
 done
 
