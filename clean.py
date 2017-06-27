c723b0d4767d5cdad005ee430b0e7b9a3f2f6d20
Zach <z.dockstader@gmail.com>
2017-06-27 11:41:33 -0400

    creating a way to automate rerunning the program

---
+++ clean.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+f = open("test.txt","r")
+read = f.readlines()
+goTo = int(read[0])
+
+i = 0
+NeedErase = False
+for line in read:
+    if i < goTo:
+        None
+    else:
+        if line[0] != " ":
+            NeedErase = True
+            break
+    i += 1
+
+if NeedErase:
+    i = 0
+    f = open("test.txt","w")
+    Write = False
+    for line in read:
+        if i < goTo-1:
+            f.write(line)
+        else:
+            if line[0] != " " and i > goTo-1:
+                Write = True
+            if Write:
+                f.write(line)
+        i += 1
+
+f.close()
+
